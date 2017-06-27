138ccb11258919d2ba8fc0f57cb8e19ec64de7e5
Joe Corcoran <joe@corcoran.io>
2017-06-27 16:50:11 +0200

    Namespace for Sidekiq

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -7,5 +7,9 @@ require 'database'
 Checker::Database.connect
 
 require 'sidekiq'
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
+end
+
 require 'build_worker'
 require 'config_worker'
