1d6b05d3e7f432a65fbec04bd8c015655d827dff
Joe Corcoran <joe@corcoran.io>
2017-06-27 15:52:04 +0200

    Database env

--- lib/database.rb
+++ lib/database.rb
@@ -9,11 +9,11 @@ module Checker
     end
 
     def self.config
-      if ENV['ENV'] == 'production'
+      if ENV['RACK_ENV'] == 'production'
         ENV['DATABASE_URL']
       else
         config = YAML.safe_load(File.read(File.expand_path('../../db/config.yml', __FILE__)))
-        config[ENV.fetch('ENV', 'development')]
+        config[ENV.fetch('RACK_ENV', 'development')]
       end
     end
   end
