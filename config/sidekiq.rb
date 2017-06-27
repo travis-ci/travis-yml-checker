9609a7ea78a8da1cd2917172dd9c981f031f33d5
Joe Corcoran <joe@corcoran.io>
2017-06-27 15:55:25 +0200

    Require sidekiq

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,11 +1,12 @@
 $: << File.expand_path('../../lib', __FILE__)
-puts 'THIS HAS BEEN REQUIRED!'
+
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require :default
 
+require 'database'
+Checker::Database.connect
+
+require 'sidekiq'
 require 'build_worker'
 require 'config_worker'
-require 'database'
-
-Checker::Database.connect
