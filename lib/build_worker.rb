a703e0bd2d613e8cff120bf89e7468c90cb12980
carlad <carla@travis-ci.org>
2017-07-03 14:05:15 +0200

    rollback

--- lib/build_worker.rb
+++ lib/build_worker.rb
@@ -5,9 +5,7 @@ module Checker
     include Sidekiq::Worker
     sidekiq_options queue: 'yml'
 
-    time = Time.now + 60
-
-    def perform_in(time, build_id, request_id)
+    def perform(build_id, request_id)
       result = Result.find_or_create_by(request_id: request_id)
       result.update_attributes(build_id: build_id)
     end
